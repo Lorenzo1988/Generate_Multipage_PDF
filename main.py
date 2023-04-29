02038377ae83e82e44616800be80b5a3253b061a
lorenzo <lorenzo.chiappetta88@gmail.com>
2023-04-29 23:39:00 +0200

    Aggiunto secondo loop per numero pagine per Topic

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 
 df = pd.read_csv("input/topics.csv",sep=",",header=0,skipinitialspace=True)
 for i, row in df.iterrows():
-        pdf.add_page()
+        num_pagine = int(row["Pages"])
+        for j in (1,num_pagine):
+                pdf.add_page()
 
-        topic = row["Topic"]
-        pdf.set_font(family="Times", style="B", size=24)
-        pdf.set_text_color(100,100,100)
-        pdf.cell(w=0, h=12, txt=f"Topic: {topic}", align="L", ln=1, border=0)
-        pdf.line(10,22,200,22)
+                topic = row["Topic"]
+                pdf.set_font(family="Times", style="B", size=24)
+                pdf.set_text_color(100,100,100)
+                pdf.cell(w=0, h=12, txt=f"Topic: {topic}. Pagina: {j}", align="L", ln=1, border=0)
+                pdf.line(10,22,200,22)
 pdf.output("output/output.pdf")
