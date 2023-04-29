8a522a98b32c85fb0d14adab130dba5f7c5e7ac9
lorenzo <lorenzo.chiappetta88@gmail.com>
2023-04-30 00:00:12 +0200

    Aggiunti footers

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 ### PANDAS INPUT
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False,margin=0)
 
 df = pd.read_csv("input/topics.csv",sep=",",header=0,skipinitialspace=True)
 for i, row in df.iterrows():
@@ -16,7 +17,18 @@ for i, row in df.iterrows():
                     pdf.add_page()
                     pdf.cell(w=0, h=12, txt=f"Topic: {topic}. Pagina: {j}", align="L", ln=1, border=0)
                     pdf.line(10,22,200,22)
+                    #Set Footer
+                    pdf.ln(265)  # aggiunti 278 millimetri vuoti e arriva a pie pagina
+
+                    pdf.set_font(family="Times", style="I", size=12)
+                    pdf.set_text_color(180,180,180)
+                    pdf.cell(w=0,h=10,txt=row["Topic"],align="R")
                 else:
                     pdf.add_page()
+                    # Set Footer
+                    pdf.ln(int(265+12))  # aggiunti 278 millimetri vuoti e arriva a pie pagina
+                    pdf.set_font(family="Times", style="I", size=12)
+                    pdf.set_text_color(180, 180, 180)
+                    pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
 pdf.output("output/output.pdf")
