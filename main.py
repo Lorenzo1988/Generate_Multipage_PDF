cf7b82c4228aeca53f1516344dade836afacbd01
lorenzo <lorenzo.chiappetta88@gmail.com>
2023-04-29 23:47:53 +0200

    Aggiunta intestazione per Topic

--- main.py
+++ main.py
@@ -6,13 +6,17 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 
 df = pd.read_csv("input/topics.csv",sep=",",header=0,skipinitialspace=True)
 for i, row in df.iterrows():
-        num_pagine = int(row["Pages"])
+        num_pagine = int(row["Pages"])-1
         for j in (1,num_pagine):
-                pdf.add_page()
-
                 topic = row["Topic"]
                 pdf.set_font(family="Times", style="B", size=24)
                 pdf.set_text_color(100,100,100)
-                pdf.cell(w=0, h=12, txt=f"Topic: {topic}. Pagina: {j}", align="L", ln=1, border=0)
-                pdf.line(10,22,200,22)
+
+                if j == 1:
+                    pdf.add_page()
+                    pdf.cell(w=0, h=12, txt=f"Topic: {topic}. Pagina: {j}", align="L", ln=1, border=0)
+                    pdf.line(10,22,200,22)
+                else:
+                    pdf.add_page()
+
 pdf.output("output/output.pdf")
