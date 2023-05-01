e5cc18624c70ac3e6eefefb36bd58cc2740f71f3
lorenzo <lorenzo.chiappetta88@gmail.com>
2023-05-01 23:30:46 +0200

    Aggiustamenti grafici codice PEP-8

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 from fpdf import FPDF
 import pandas as pd
 
+K= 0
+SOGLIA_FOOTER = 265
+
 ### PANDAS INPUT
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 pdf.set_auto_page_break(auto=False,margin=0)
-
 df = pd.read_csv("input/topics.csv",sep=",",header=0,skipinitialspace=True)
-k= 0
-soglia_footer = 265
+
+
+
 
 def inserimento_righe(posizione_riga=22, altezza_riga_mm = 10):
-    global soglia_footer
-    while posizione_riga <= soglia_footer:
+    """
+    Funzione che permette di inserire tutte le righe sul foglio
+    bianco distanziate di una misura <altezza_riga_mm"""
+    global SOGLIA_FOOTER
+    while posizione_riga <= SOGLIA_FOOTER:
         pdf.line(10, posizione_riga, 200, posizione_riga)
         posizione_riga = posizione_riga + altezza_riga_mm
 
@@ -26,25 +32,24 @@ for i, row in df.iterrows():
                 pdf.set_text_color(100,100,100)
                 j=j+1
                 if j == 1:
-                    k=k+1
-                    print(f"K:{k}")
+                    K=K+1
                     pdf.add_page()
                     pdf.cell(w=0, h=12, txt=f"Topic: {topic}. Pagina: {j}", align="L", ln=1, border=0)
 
                     inserimento_righe()
                     #Set Footer
-                    pdf.ln(soglia_footer)  # aggiunti 278 millimetri vuoti e arriva a pie pagina
+                    # aggiunti 278 millimetri vuoti e arriva a pie pagina
+                    pdf.ln(SOGLIA_FOOTER)
                     pdf.set_font(family="Times", style="I", size=12)
                     pdf.set_text_color(180,180,180)
                     pdf.cell(w=0,h=10,txt=row["Topic"]+". Pagina:"+str(k),align="R")
                 else:
-                    k=k+1
-                    print(f"K:{k}")
+                    K=K+1
                     pdf.add_page()
                     inserimento_righe()
 
                     # Set Footer
-                    pdf.ln(int(265+12))  # aggiunti 278 millimetri vuoti e arriva a pie pagina
+                    pdf.ln(int(SOGLIA_FOOTER+12))
                     pdf.set_font(family="Times", style="I", size=12)
                     pdf.set_text_color(180, 180, 180)
                     pdf.cell(w=0, h=10, txt=row["Topic"]+". Pagina:"+str(k), align="R")
