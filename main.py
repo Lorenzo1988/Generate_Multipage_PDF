a33e4c55d87e194f2d1da07bb25f47852a337929
lorenzo <lorenzo.chiappetta88@gmail.com>
2023-05-01 12:38:36 +0200

    Aggiunti numeri pagina a footers

--- main.py
+++ main.py
@@ -6,29 +6,32 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 pdf.set_auto_page_break(auto=False,margin=0)
 
 df = pd.read_csv("input/topics.csv",sep=",",header=0,skipinitialspace=True)
+k= 0
 for i, row in df.iterrows():
         num_pagine = int(row["Pages"])-1
         for j in (1,num_pagine):
                 topic = row["Topic"]
                 pdf.set_font(family="Times", style="B", size=24)
                 pdf.set_text_color(100,100,100)
-
                 if j == 1:
+                    k=k+1
+                    print(f"K:{k}")
                     pdf.add_page()
                     pdf.cell(w=0, h=12, txt=f"Topic: {topic}. Pagina: {j}", align="L", ln=1, border=0)
                     pdf.line(10,22,200,22)
                     #Set Footer
                     pdf.ln(265)  # aggiunti 278 millimetri vuoti e arriva a pie pagina
-
                     pdf.set_font(family="Times", style="I", size=12)
                     pdf.set_text_color(180,180,180)
-                    pdf.cell(w=0,h=10,txt=row["Topic"],align="R")
+                    pdf.cell(w=0,h=10,txt=row["Topic"]+". Pagina:"+str(k),align="R")
                 else:
+                    k=k+1
+                    print(f"K:{k}")
                     pdf.add_page()
                     # Set Footer
                     pdf.ln(int(265+12))  # aggiunti 278 millimetri vuoti e arriva a pie pagina
                     pdf.set_font(family="Times", style="I", size=12)
                     pdf.set_text_color(180, 180, 180)
-                    pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+                    pdf.cell(w=0, h=10, txt=row["Topic"]+". Pagina:"+str(k), align="R")
 
 pdf.output("output/output.pdf")
