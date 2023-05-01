9a60799d42955007136b7721244ec79869234e01
lorenzo <lorenzo.chiappetta88@gmail.com>
2023-05-01 22:26:04 +0200

    Modifica al main.py

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 pdf.set_auto_page_break(auto=False,margin=0)
 
 df = pd.read_csv("input/topics.csv",sep=",",header=0,skipinitialspace=True)
+
+# k serve per le note a piede (footer)
 k= 0
 for i, row in df.iterrows():
         num_pagine = int(row["Pages"])-1
@@ -13,21 +15,18 @@ for i, row in df.iterrows():
                 topic = row["Topic"]
                 pdf.set_font(family="Times", style="B", size=24)
                 pdf.set_text_color(100,100,100)
+                k = k+1
+                print(f"K:{k}")
+                pdf.add_page()
                 if j == 1:
-                    k=k+1
-                    print(f"K:{k}")
-                    pdf.add_page()
                     pdf.cell(w=0, h=12, txt=f"Topic: {topic}. Pagina: {j}", align="L", ln=1, border=0)
                     pdf.line(10,22,200,22)
-                    #Set Footer
+                   #Set Footer
                     pdf.ln(265)  # aggiunti 278 millimetri vuoti e arriva a pie pagina
                     pdf.set_font(family="Times", style="I", size=12)
                     pdf.set_text_color(180,180,180)
                     pdf.cell(w=0,h=10,txt=row["Topic"]+". Pagina:"+str(k),align="R")
                 else:
-                    k=k+1
-                    print(f"K:{k}")
-                    pdf.add_page()
                     # Set Footer
                     pdf.ln(int(265+12))  # aggiunti 278 millimetri vuoti e arriva a pie pagina
                     pdf.set_font(family="Times", style="I", size=12)
