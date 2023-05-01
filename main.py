8c22392caa029c81b07fb685a9db562bcd8d25ad
lorenzo <lorenzo.chiappetta88@gmail.com>
2023-05-01 23:06:46 +0200

    Inserimento righe sui fogli. Riaggiustamento topics.csv

--- main.py
+++ main.py
@@ -6,27 +6,43 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 pdf.set_auto_page_break(auto=False,margin=0)
 
 df = pd.read_csv("input/topics.csv",sep=",",header=0,skipinitialspace=True)
-
-# k serve per le note a piede (footer)
 k= 0
+soglia_footer = 265
+
+def inserimento_righe(posizione_riga=22, altezza_riga_mm = 10):
+    global soglia_footer
+    while posizione_riga <= soglia_footer:
+        pdf.line(10, posizione_riga, 200, posizione_riga)
+        posizione_riga = posizione_riga + altezza_riga_mm
+
+
 for i, row in df.iterrows():
-        num_pagine = int(row["Pages"])-1
-        for j in (1,num_pagine):
+        num_pagine = int(row["Pages"])
+        print(f"num_pagine: {num_pagine}")
+        j=0
+        while j < num_pagine:
                 topic = row["Topic"]
                 pdf.set_font(family="Times", style="B", size=24)
                 pdf.set_text_color(100,100,100)
-                k = k+1
-                print(f"K:{k}")
-                pdf.add_page()
+                j=j+1
                 if j == 1:
+                    k=k+1
+                    print(f"K:{k}")
+                    pdf.add_page()
                     pdf.cell(w=0, h=12, txt=f"Topic: {topic}. Pagina: {j}", align="L", ln=1, border=0)
-                    pdf.line(10,22,200,22)
-                   #Set Footer
-                    pdf.ln(265)  # aggiunti 278 millimetri vuoti e arriva a pie pagina
+
+                    inserimento_righe()
+                    #Set Footer
+                    pdf.ln(soglia_footer)  # aggiunti 278 millimetri vuoti e arriva a pie pagina
                     pdf.set_font(family="Times", style="I", size=12)
                     pdf.set_text_color(180,180,180)
                     pdf.cell(w=0,h=10,txt=row["Topic"]+". Pagina:"+str(k),align="R")
                 else:
+                    k=k+1
+                    print(f"K:{k}")
+                    pdf.add_page()
+                    inserimento_righe()
+
                     # Set Footer
                     pdf.ln(int(265+12))  # aggiunti 278 millimetri vuoti e arriva a pie pagina
                     pdf.set_font(family="Times", style="I", size=12)
---
+++ q
@@ -0,0 +1,10 @@
+[33m9a60799[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m Modifica al main.py
+[33mf88e6fd[m Cancellato nuovo_file.txt
+[33mc433b94[m Nuove modifiche al nuovo_file.txt
+[33m3c3905d[m Commit inutile2 di aggiunta riga, da cancellare
+[33mc077aba[m Commit inutile di aggiunta riga, da cancellare
+[33ma33e4c5[m Aggiunti numeri pagina a footers
+[33m6e25a92[m Cambio file output.pdf
+[33m8a522a9[m Aggiunti footers
+[33mcf7b82c[m Aggiunta intestazione per Topic
+[33m0203837[m Aggiunto secondo loop per numero pagine per Topic
